5908745444146d2b36b0ceef85814787eb580a37
shaye <sm0548540902@gmail.com>
2025-10-23 12:44:45 +0300

    Deleting what is not needed

--- game_logic/game.py
+++ game_logic/game.py
@@ -5,6 +5,7 @@ def create_player(name = "AI"):
     n_play["hand"] = []
     n_play["won_plie"] = []
     return n_play
+
 def init_game():
     p1 = create_player("shaye")
     p2 = create_player()
@@ -14,7 +15,7 @@ def init_game():
     p1["hand"]+=deck[0:26]
     p2["hand"]+=deck[26:]
     return {"deck":deck,"player_1":p1,"player_2":p2}
-    print(p2["hand"])
+
 def play_round(p1:dict,p2:dict):
     for i in range(len(p1["hand"])):
         win = compare_cards(p1["hand"][0],p2["hand"][0])
@@ -27,6 +28,10 @@ def play_round(p1:dict,p2:dict):
             p2["won_plie"].append(p2["hand"].pop(0))
             print("hand:", i, p2["name"], "win")
         else:
-            print(win)
-game = init_game()
-play_round(game["player_1"],game["player_2"])
+            print("hand",i,win)
+    if len(p1["won_plie"]) > len(p2["won_plie"]):
+            print(p1["name"],"win")
+    elif len(p1["won_plie"]) < len(p2["won_plie"]):
+            print(p2["name"], "win")
+    else:
+        print("draw")
